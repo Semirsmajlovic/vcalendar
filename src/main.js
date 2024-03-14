cc61ad931a263fc26313dd924c3980a5b078319a
Semir Smajlovic <Semirxbih@gmail.com>
2024-03-14 06:41:16 -0500

    031424

--- src/main.js
+++ src/main.js
@@ -1,8 +1,6 @@
 import Vue from 'vue';
 import App from './App.vue';
 import vuetify from './plugins/vuetify';
-
-
 import store from './store';
 import router from './routes/index';
 import { initializeApp } from "firebase/app";
@@ -30,13 +28,13 @@ Vue.config.productionTip = false;
 let app;
 
 getAuth().onAuthStateChanged(user => {
-  store.dispatch('setUser', user); // Dispatch action to update user state
-  if (!app) {
+  store.dispatch('setUser', user); // Dispatches an action to the Vuex store to update the user state with the current user
+  if (!app) { // Checks if the Vue instance hasn't been created yet
     app = new Vue({
-      vuetify,
-      store,
-      router,
-      render: h => h(App, { props: { isLoggedIn: !!user } }),
-    }).$mount('#app');
+      vuetify, // Integrates Vuetify for UI components
+      store, // Includes Vuex store for state management
+      router, // Includes Vue Router for navigation
+      render: h => h(App, { props: { isLoggedIn: !!user } }), // Renders the App component and passes isLoggedIn prop based on user's authentication status
+    }).$mount('#app'); // Mounts the Vue instance to the DOM element with id 'app'
   }
 });
